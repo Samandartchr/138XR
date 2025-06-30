64e1ad5de11ae2d944e8b59e54e4254ffecdf7e3
Samandartchr <Szhalishov@gmail.com>
2025-06-30 22:12:31 +0500

    Initial commit of SimpleCircuit class with node, wire, and power source definitions.

---
+++ SimpleCircuit.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public class SimpleCircuit : MonoBehaviour
+{
+    public List<GameObject> nodes;
+    public List<GameObject> wires;
+    public GameObject powerSource;
+}
